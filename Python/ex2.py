4fae124fc8d96ea089c8f1fcaadcd39209c12d81
Harshveer Gaba <harsh.gaba56@gmail.com>
2020-02-03 20:13:06 -0500

    Uploading edit of ex2.py

--- Python/ex2.py
+++ Python/ex2.py
@@ -1,2 +1,12 @@
 # ProjectName - ex2.py
 # @author - Harshveer Gaba
+# @date - February 3, 2020
+
+print("Code line 1 after comment.")
+
+# Middle comment testing
+# <- is called octothorpe
+# <- comments or disables the lines even code lines
+# print("Hello World!") <- this won't execute
+
+print("But this would!")
