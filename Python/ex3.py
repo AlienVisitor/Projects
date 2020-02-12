4e5e9998a33a4e416ec1bbac31bd9ae3d542bbbd
The_Spectator <harsh.gaba56@gmail.com>
2020-02-12 18:34:45 -0500

    Added ex3.py

---
+++ Python/ex3.py
@@ -0,0 +1,37 @@
+# Prints a string
+print("I will now count my chickens:")
+
+# Calculates 30 / 6, then calculates 25 + 5.0
+print("Hens", 25 + 30 / 6)
+# Calculates 25 * 3, then calculates 75 % 4, then calculates 100 - 3
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Prints a string
+print("Now I will count the eggs:")
+
+# Calculates 1 / 4, then calculates 4 % 2, then calculates 3 + 2 + 1 - 5 - 0.25 + 6
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Prints a string
+print("Is it true that 3 + 2 < 5 - 7")
+
+# Calculates 3 + 2 and 5 - 7, then compares 5 < -2
+print(3 + 2 < 5 - 7)
+
+# Prints a string and calculates 3 + 2
+print("What is 3 + 2?", 3 + 2)
+# Prints a string and calculates 5 - 7
+print("What is 5 - 7?", 5 - 7)
+
+# Prints a string
+print("Oh, that's why it's False.")
+
+# Prints a string
+print("How about some more.")
+
+# Prints a string and compares 5 > -2
+print("Is it greater?", 5 > -2)
+# Prints a string and compares 5 >= -2
+print("Is it greater or equal?", 5 >= -2)
+# Prints a string and compares 5 <= -2
+print("Is it less or equal?", 5 <= -2)
