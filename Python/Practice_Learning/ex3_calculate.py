1aad2fe64232f57757cda76249ed33e93eb34793
The_Spectator <harsh.gaba56@gmail.com>
2020-02-12 18:42:04 -0500

    Renamed the folder and added ex3_calculate.py

---
+++ Python/Practice_Learning/ex3_calculate.py
@@ -0,0 +1,12 @@
+# Prints a string
+print("What should we calculate?")
+# Prints a string
+print("Maybe a basic 2 + 2? :P")
+# Prints a string
+print("But with a twist!!")
+
+# Type casting to float!
+print(float(2 + 2))
+
+# Not casted to float!
+print(2 + 2)
